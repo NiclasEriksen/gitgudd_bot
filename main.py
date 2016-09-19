537e288a4c7f8aae0c7d5b144201b5554817717d
Niclas Eriksen <niclaseriksen@gmx.com>
2016-09-19 20:27:17 +0200

    Igjen.

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ async def on_message(message):
         async for msg in client.logs_from(
             message.channel, limit=1, before=message
         ):
-            newmsg = msg.content[::-1]
+            newmsg = "*{0}*\n{1}".format(msg.authour, msg.content[::-1])
             await client.delete_message(msg)
         await client.send_message(message.channel, newmsg)
         await client.delete_message(message)
