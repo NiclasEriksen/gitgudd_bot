c15a26dddf6cac2151f87cfe78214cf4bf041b68
Niclas Eriksen <niclaseriksen@gmx.com>
2017-02-01 15:45:24 +0100

    boom

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ async def commit_checker():
         if not cstamp == stamp:
             cache.put(cache="git_stamps", key="commit", value=stamp)
         if gh_obj:
-            async for log in client.logs_from(channel, limit=20):
+            async for log in client.logs_from(message.channel, limit=20):
                 for e in log.embeds:
                     if e.url == gh_obj["url"]:
                         print("Commit already posted, abort!")
